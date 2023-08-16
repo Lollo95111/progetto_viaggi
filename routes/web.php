73dbd60c3f3f9ff630df7423c61369e228d8147c
Lorenzo Ricciardiello <lorenzo1995111@gmail.com>
2023-08-16 20:20:51 +0100

    portatile

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\myController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [myController::class,'welcome'])->name('Home');
